df26f40fd4264a8253dcc33052d86b7a10d1390c
Code Sandbox <hello@codesandbox.io>
2023-02-20 10:21:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e9516e539f19308919413c63103e4bc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424c4c69bd56eeaf769eaf35a6eebcb6",
+    "url": "https://jobet1995.github.io/csb-qfvkfy/index.html"
+  },
+  {
+    "revision": "adfc4954b095be271569",
+    "url": "https://jobet1995.github.io/csb-qfvkfy/static/js/2.dcff96ce.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://jobet1995.github.io/csb-qfvkfy/static/js/2.dcff96ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7446fcbcc3ab8ca14a23",
+    "url": "https://jobet1995.github.io/csb-qfvkfy/static/js/main.f74e8b46.chunk.js"
+  },
+  {
+    "revision": "620340b66135d1188ad1",
+    "url": "https://jobet1995.github.io/csb-qfvkfy/static/js/runtime-main.d943a487.js"
+  }
+]);
